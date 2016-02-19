40ffe4e382ecc11528824c52a462c8ff2c8d957b
daniel lirio reis <daniellirioreis@gmail.com>
2016-02-19 01:45:37 -0300

    adicionando linha para resolver error no heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ ImaTeste::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+	config.assets.js_compressor = :uglifier
 end
