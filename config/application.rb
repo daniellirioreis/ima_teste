6005bd8a929071b6f113c3d4f133c11d677b2c4d
Daniel Lirio Reis <daniellirioreis@gmail.com>
2016-04-13 00:17:30 -0300

    alteração de css

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module ImaTeste
     config.time_zone = 'Brasilia'
     config.i18n.default_locale = 'pt-BR'
 		config.autoload_paths << "#{Rails.root}/app/enumerations"
+	  config.assets.precompile = config.assets.precompile + %w(*.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg *.eot)
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
